c7ae2e1613608f3631e6c5784defa732b54d9503
CodeMath <xyrho123@naver.com>
2024-04-13 00:47:23 +0900

    BUG : ENUM 타입 오류...발생 뭐가 문제일지 모르겠음. Ref #1

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -9,35 +9,35 @@ export class BoardsController {
     constructor(private boardsService: BoardsService) { }
 
     @Get()
-    getAllBoard(): Board[] {
+    async getAllBoard(): Promise<Board[]> {
         return this.boardsService.getAllBoards();
     }
 
-    @Post() // 요청을 보낼 때, 정보를 받고 처리해야하므로,
-    @UsePipes(ValidationPipe) // 유효성 검증 파이프
-    createBoard(
-        @Body() CreateBoardDto: CreateBoardDto
-    ): Board {
+    // @Post() // 요청을 보낼 때, 정보를 받고 처리해야하므로,
+    // @UsePipes(ValidationPipe) // 유효성 검증 파이프
+    // createBoard(
+    //     @Body() CreateBoardDto: CreateBoardDto
+    // ): Board {
 
-        return this.boardsService.createBoard(CreateBoardDto);
-    }
+    //     return this.boardsService.createBoard(CreateBoardDto);
+    // }
 
-    @Get('/:id')
-    getBoardById(@Param('id') id: string): Board{
-        return this.boardsService.getBoardById(id);
-    }
+    // @Get('/:id')
+    // getBoardById(@Param('id') id: string): Board{
+    //     return this.boardsService.getBoardById(id);
+    // }
 
-    @Delete('/:id')
-    deleteBoard(@Param('id') id: string){
-        return this.boardsService.deleteBoard(id);
-    }
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id: string){
+    //     return this.boardsService.deleteBoard(id);
+    // }
 
-    @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id: string, 
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board{
-        return this.boardsService.updateBoardStatus(id, status);
-    }
+    // @Patch('/:id/status')
+    // updateBoardStatus(
+    //     @Param('id') id: string, 
+    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board{
+    //     return this.boardsService.updateBoardStatus(id, status);
+    // }
 
 
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,48 +2,55 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.models';
 import { v1 as uuid } from 'uuid'; // v1 버전을 사용한다
 import { CreateBoardDto } from './dto/create-board.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class BoardsService {
-    // 다른 모듈에서 수정하지 못하도록 private
-    private boards: Board[] = [];
+
+    constructor(private readonly prisma: PrismaService){} // 전역 PrismaService
+
+    // // 다른 모듈에서 수정하지 못하도록 private
+    // private boards: Board[] = [];
 
     // 전체 게시글 가져오기
-    getAllBoards(): Board[] {
-        return this.boards;
+    // getAllBoards(): Board[] {
+    //     return this.boards;
+    // }
+    async getAllBoards(): Promise<Board[]> {
+        return await this.prisma.board.findMany(); // ENUM 타입이 다르다고 하는데...
     }
 
-    createBoard(CreateBoardDto: CreateBoardDto){
-        const {title, description} = CreateBoardDto;
+    // createBoard(CreateBoardDto: CreateBoardDto){
+    //     const {title, description} = CreateBoardDto;
 
-        const board: Board = {
-            id: uuid(), // DB에서는 알아서 처리하지만, 로컬 메모리에서는 uuid 쓰자
-            title,
-            description,
-            status: BoardStatus.PUBLIC
-        }
-        this.boards.push(board);
-        return board;
-    };
+    //     const board: Board = {
+    //         id: uuid(), // DB에서는 알아서 처리하지만, 로컬 메모리에서는 uuid 쓰자
+    //         title,
+    //         description,
+    //         status: BoardStatus.PUBLIC
+    //     }
+    //     this.boards.push(board);
+    //     return board;
+    // };
 
-    getBoardById(id: string): Board{
-        const found = this.boards.find((board) => board.id === id );
+    // getBoardById(id: string): Board{
+    //     const found = this.boards.find((board) => board.id === id );
 
-        if(!found){
-            throw new NotFoundException(`can't find Board with id ${id}`); // 404 
-        }
+    //     if(!found){
+    //         throw new NotFoundException(`can't find Board with id ${id}`); // 404 
+    //     }
 
-        return found;
-    }
+    //     return found;
+    // }
 
-    deleteBoard(id: string){
-        const found = this.getBoardById(id);
-        this.boards = this.boards.filter(board => board.id !== found.id);
-    }
+    // deleteBoard(id: string){
+    //     const found = this.getBoardById(id);
+    //     this.boards = this.boards.filter(board => board.id !== found.id);
+    // }
 
-    updateBoardStatus(id:string, status:BoardStatus): Board{
-        const board = this.getBoardById(id);
-        board.status = status;
-        return board;
-    }
+    // updateBoardStatus(id:string, status:BoardStatus): Board{
+    //     const board = this.getBoardById(id);
+    //     board.status = status;
+    //     return board;
+    // }
 }
