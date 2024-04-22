b656d59343cf7444ff754ef5e0216ff7eaed84af
CodeMath <xyrho123@naver.com>
2024-04-23 00:13:10 +0900

    Feat : jwt/passport 추가
    1. 로그인 시 accessToken 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { UserEntity } from './entities/user.entity';
-import { ApiBody, ApiCreatedResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiCreatedResponse, ApiOkResponse, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { UserDto } from './dto/create-user.dto';
 import { UserCreateValidationPipe } from './pipes/user-create-validate.pipe';
@@ -13,11 +13,17 @@ export class AuthController {
 
     @ApiBody({type: UserDto})
     @ApiCreatedResponse({status:201, description:"new ok", type: UserEntity})
-    @Post()
-    async createUser(@Body() userDto: UserDto){
+    @Post('/signup')
+    async createUser(@Body() userDto: UserDto): Promise<UserEntity>{
         return await this.authsService.createUser(userDto);
     }
     
-
+    @ApiBody({type: UserDto})
+    @ApiOkResponse({status: 200, description: "ok"})
+    @ApiUnauthorizedResponse({status: 401, description:"비밀번호 틀림"})
+    @Post('/signin')
+    async signInUser(@Body() userDto: UserDto): Promise<{accessToken: string}>{
+        return await this.authsService.signInUser(userDto);
+    }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,15 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private prisma: PrismaService
+        private prisma: PrismaService, 
+        private jwtService: JwtService
     ) { };
 
 
@@ -25,5 +27,26 @@ export class AuthService {
         });
         
         return user;
+    }
+
+    async signInUser(data: UserDto){
+        const user = await this.prisma.user.findUniqueOrThrow({
+            where: {
+                email: data.email
+            } 
+        }).catch(error => {
+            throw new NotFoundException(`${error}`); 
+        });
+        
+        if(user && (await bcrypt.compare(data.password, user.password))){
+            // 유저 토큰 생성(Secret + Payload )
+            const payload = { email: user.email } ;
+            const accessToken = await this.jwtService.sign(payload);
+
+            return {accessToken: accessToken}
+        }else{
+            throw new UnauthorizedException("Invalid password");
+        }
+
     }
 }
