4a3291b88135c4d81c7eeb1d92fa48b2a672fedc
CodeMath <xyrho123@naver.com>
2024-04-22 22:11:22 +0900

    Feat : 회원가입 API 구현
    1. bcrypt 통해 암호화 처리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
-
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -12,10 +12,18 @@ export class AuthService {
 
 
     async createUser(data: UserDto): Promise<User | never> {
-        return await this.prisma.user.create({
-            data: data
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(data.password, salt);
+
+        const user = this.prisma.user.create({
+            data: {
+                email: data.email,
+                password: hashedPassword
+            }
         }).catch(error=>{
             throw new BadRequestException(`${error}`); 
-        })
+        });
+        
+        return user;
     }
 }
